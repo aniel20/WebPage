64ebdae6c3551dee7380e6a0998501f969052c2d
anisoara1 <158809127+aniel20@users.noreply.github.com>
2024-02-07 09:25:14 +0200

    Update theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,72 +1,36 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import React, { useState } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Switch } from '@mui/material';
 import Header from './Navbar/Navbar';
+import css from '../components/App.module.css';
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+export default function App() {
+  const [toggleDarkMode, setToggleDarkMode] = useState(true);
 
-function App() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      {theme.palette.mode} mode
-      <IconButton
-        sx={{ ml: 1 }}
-        onClick={colorMode.toggleColorMode}
-        color="inherit"
-      >
-        {theme.palette.mode === 'dark' ? (
-          <Brightness7Icon />
-        ) : (
-          <Brightness4Icon />
-        )}
-      </IconButton>
-    </Box>
-  );
-}
+  const toggleDarkTheme = () => {
+    setToggleDarkMode(!toggleDarkMode);
+  };
 
-export default function ToggleColorMode() {
-  const [mode, setMode] = React.useState('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+  const darkTheme = createTheme({
+    palette: {
+      mode: toggleDarkMode ? 'dark' : 'light',
+      primary: {
+        main: '#90caf9',
       },
-    }),
-    []
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode]
-  );
+      secondary: {
+        main: '#131052',
+      },
+    },
+  });
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <App />
-        <Header />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Header />
+      <div className={css.swichPosition}>
+        <Switch checked={toggleDarkMode} onChange={toggleDarkTheme} />
+      </div>
+    </ThemeProvider>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,70 +1,61 @@
 import React from 'react';
 import css from './Navbar.module.css';
-
 import { Box, MenuItem, MenuList, Typography, Link } from '@mui/material';
-import { useTheme, ThemeProvider } from '@mui/material';
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 const Header = () => {
-  const colorMode = React.useContext(ColorModeContext);
-  const theme = useTheme();
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <Box
-          sx={{
-            bgcolor: 'background.default',
-            color: 'text.primary',
-          }}
-          className={css.navbar}
-        >
-          <Link sx={{ textDecoration: 'none', color: 'text.primary' }} to="/">
-            <h1 className={css.title}>
-              <span className={css.titleBlue}>Web</span>Studio
-            </h1>
-          </Link>
-          <MenuList
-            sx={{
-              padding: '0',
-              display: 'flex',
-              flexDirection: 'row',
-              gap: '20px',
-              bgcolor: 'background.default',
-              color: 'text.primary',
-            }}
-          >
-            <MenuItem>
-              <Typography className={css.menuItem}>Studio</Typography>
-            </MenuItem>
-            <MenuItem>
-              <Typography className={css.menuItem}>Portofolio</Typography>
-            </MenuItem>
-            <MenuItem>
-              <Typography className={css.menuItem}>Contacts</Typography>
-            </MenuItem>
-          </MenuList>
+    <Box
+      sx={{
+        bgcolor: 'background.default',
+        color: 'text.primary',
+      }}
+      className={css.navbar}
+    >
+      <Link sx={{ textDecoration: 'none', color: 'text.primary' }} to="/">
+        <h1 className={css.title}>
+          <span className={css.titleBlue}>Web</span>Studio
+        </h1>
+      </Link>
+      <MenuList
+        sx={{
+          padding: '0',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '20px',
+          bgcolor: 'background.default',
+          color: 'text.primary',
+        }}
+      >
+        <MenuItem>
+          <Typography className={css.menuItem}>Studio</Typography>
+        </MenuItem>
+        <MenuItem>
+          <Typography className={css.menuItem}>Portofolio</Typography>
+        </MenuItem>
+        <MenuItem>
+          <Typography className={css.menuItem}>Contacts</Typography>
+        </MenuItem>
+      </MenuList>
 
-          <MenuList
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              gap: '20px',
-              bgcolor: 'background.default',
-              color: 'text.primary',
-            }}
-          >
-            <MenuItem>
-              <Link className={css.menuItem} sx={{ textDecoration: 'none' }}>
-                info@devstudio.com
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link sx={{ textDecoration: 'none' }}>+1 212-772-4150</Link>
-            </MenuItem>
-          </MenuList>
-        </Box>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+      <MenuList
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '20px',
+          bgcolor: 'background.default',
+          color: 'text.primary',
+        }}
+      >
+        <MenuItem>
+          <Link className={css.menuItem} sx={{ textDecoration: 'none' }}>
+            info@devstudio.com
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link sx={{ textDecoration: 'none' }}>+1 212-772-4150</Link>
+        </MenuItem>
+      </MenuList>
+    </Box>
   );
 };
 
